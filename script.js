c8cd5a7edc3a41804754b693f5828153e79a6c5c
Jason Yoh <jasonyoh@Jasons-MacBook-Pro.local>
2016-10-29 11:30:52 -0500

    first commit.basic functionality for git example

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log( 'script.js sourced' );
+
+var buttonClick = function(){
+  alert( 'you clicked on the button' )
+}; // end funk
+
+var showAlert = function(){
+  alert( 'Something HAPPENED!!!!' )
+}; // end funk
